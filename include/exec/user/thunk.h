6171d82516b151c7d2bac6484c801c45d8de796e
Alexander Graf <agraf@suse.de>
2009-04-14 16:20:50 +0200

    qemu-cvs-alsa_bitfield
    Implements TYPE_INTBITFIELD partially. (required for ALSA support)
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Ulrich Hecht <uli@suse.de>

--- include/exec/user/thunk.h
+++ include/exec/user/thunk.h
@@ -37,6 +37,7 @@ typedef enum argtype {
     TYPE_ARRAY,
     TYPE_STRUCT,
     TYPE_OLDDEVT,
+    TYPE_INTBITFIELD,
 } argtype;
 
 #define MK_PTR(type) TYPE_PTR, type
@@ -90,6 +91,7 @@ static inline int thunk_type_size(const argtype *type_ptr, int is_host)
     case TYPE_SHORT:
         return 2;
     case TYPE_INT:
+    case TYPE_INTBITFIELD:
         return 4;
     case TYPE_LONGLONG:
     case TYPE_ULONGLONG:
@@ -152,6 +154,7 @@ static inline int thunk_type_align(const argtype *type_ptr, int is_host)
     case TYPE_SHORT:
         return 2;
     case TYPE_INT:
+    case TYPE_INTBITFIELD:
         return 4;
     case TYPE_LONGLONG:
     case TYPE_ULONGLONG:
