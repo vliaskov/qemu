652bb814f5755ca4280672c9a198f84266349673
Alexander Graf <agraf@suse.de>
2013-02-22 03:48:03 +0100

    fix up pc on segfaults correctly

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -10239,6 +10239,10 @@ void arm_cpu_dump_state(CPUState *cs, FILE *f, fprintf_function cpu_fprintf,
 
 void restore_state_to_opc(CPUARMState *env, TranslationBlock *tb, int pc_pos)
 {
-    env->regs[15] = tcg_ctx.gen_opc_pc[pc_pos];
+    if (is_a64(env)) {
+        env->pc = tcg_ctx.gen_opc_pc[pc_pos];
+    } else {
+        env->regs[15] = tcg_ctx.gen_opc_pc[pc_pos];
+    }
     env->condexec_bits = gen_opc_condexec_bits[pc_pos];
 }
