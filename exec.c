f18e9ab50fb4de977cf834c0aebbb016d07bcd93
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2015-03-16 22:35:54 -0700

    exec: Respect as_tranlsate_internal length clamp
    address_space_translate_internal will clamp the *plen length argument
    based on the size of the memory region being queried. The iommu walker
    logic in addresss_space_translate was ignoring this by discarding the
    post fn call value of *plen. Fix by just always using *plen as the
    length argument throughout the fn, removing the len local variable.
    
    This fixes a bootloader bug when a single elf section spans multiple
    QEMU memory regions.
    
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Message-Id: <1426570554-15940-1-git-send-email-peter.crosthwaite@xilinx.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit c3c1bb99d1c11978d9ce94d1bdcf0705378c1459)
    [BR: BSC#969121 CVE-2015-8817]
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- exec.c
+++ exec.c
@@ -363,7 +363,6 @@ MemoryRegion *address_space_translate(AddressSpace *as, hwaddr addr,
     IOMMUTLBEntry iotlb;
     MemoryRegionSection *section;
     MemoryRegion *mr;
-    hwaddr len = *plen;
 
     for (;;) {
         section = address_space_translate_internal(as->dispatch, addr, &addr, plen, true);
@@ -376,7 +375,7 @@ MemoryRegion *address_space_translate(AddressSpace *as, hwaddr addr,
         iotlb = mr->iommu_ops->translate(mr, addr);
         addr = ((iotlb.translated_addr & ~iotlb.addr_mask)
                 | (addr & iotlb.addr_mask));
-        len = MIN(len, (addr | iotlb.addr_mask) - addr + 1);
+        *plen = MIN(*plen, (addr | iotlb.addr_mask) - addr + 1);
         if (!(iotlb.perm & (1 << is_write))) {
             mr = &io_mem_unassigned;
             break;
@@ -387,10 +386,9 @@ MemoryRegion *address_space_translate(AddressSpace *as, hwaddr addr,
 
     if (xen_enabled() && memory_access_is_direct(mr, is_write)) {
         hwaddr page = ((addr & TARGET_PAGE_MASK) + TARGET_PAGE_SIZE) - addr;
-        len = MIN(page, len);
+        *plen = MIN(page, *plen);
     }
 
-    *plen = len;
     *xlat = addr;
     return mr;
 }
