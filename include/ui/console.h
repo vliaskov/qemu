64c69285c91573b32fadf90e341505fae947f74e
Wolfgang Bumiller <w.bumiller@proxmox.com>
2016-01-13 09:09:58 +0100

    hmp: fix sendkey out of bounds write (CVE-2015-8619)
    When processing 'sendkey' command, hmp_sendkey routine null
    terminates the 'keyname_buf' array. This results in an OOB
    write issue, if 'keyname_len' was to fall outside of
    'keyname_buf' array.
    
    Since the keyname's length is known the keyname_buf can be
    removed altogether by adding a length parameter to
    index_from_key() and using it for the error output as well.
    
    Reported-by: Ling Liu <liuling-it@360.cn>
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
    Message-Id: <20160113080958.GA18934@olga>
    [Comparison with "<" dumbed down, test for junk after strtoul()
    tweaked]
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    
    (cherry picked from commit 64ffbe04eaafebf4045a3ace52a360c14959d196)
    [BR: BSC#960334 CVE-2015-8619]
    Signed-off-by: Bruce Rogers <brogers@suse.com>
    
    Conflicts:
    	hmp.c
            ui/input-legacy.c

--- include/ui/console.h
+++ include/ui/console.h
@@ -339,7 +339,7 @@ static inline int vnc_display_pw_expire(DisplayState *ds, time_t expires)
 void curses_display_init(DisplayState *ds, int full_screen);
 
 /* input.c */
-int index_from_key(const char *key);
+int index_from_key(const char *key, size_t key_length);
 
 /* gtk.c */
 void early_gtk_display_init(void);
