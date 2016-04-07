afcb3aa75e902e60dd48bdcf11ec0ac9b5ea14b1
Prasad J Pandit <pjp@fedoraproject.org>
2016-04-07 04:27:00 -0600

    net: mipsnet: check packet length against buffer
    When receiving packets over MIPSnet network device, it uses
     receive buffer of size 1514 bytes. In case the controller
    accepts large(MTU) packets, it could lead to memory corruption.
    Add check to avoid it.
    
    Reported by: Oleksandr Bazhaniuk <oleksandr.bazhaniuk@intel.com>
    
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    [BR: BSC#975136 CVE-2016-4002]
    Signed-off-by: Bruce Rogers <brogers@suse.com>

--- hw/net/mipsnet.c
+++ hw/net/mipsnet.c
@@ -82,6 +82,9 @@ static ssize_t mipsnet_receive(NetClientState *nc, const uint8_t *buf, size_t si
     if (!mipsnet_can_receive(nc))
         return -1;
 
+    if (size >= sizeof(s->rx_buffer)) {
+        return 0;
+    }
     s->busy = 1;
 
     /* Just accept everything. */
