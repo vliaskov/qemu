1fdd600e5054c022171888ebc23b0baba9ea9004
Fabiano Rosas <farosas@suse.de>
2024-08-16 10:25:10 -0300

    [openSUSE] fixup patch "pc: q35: Allow 1024 cpus for old machine types"
    Remove spurious initialization with PC_MACHINE_CLASS().
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    [DF: added some context in the changelog]
    Signed-off-by: Dario Faggioli <dfaggioli@suse.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -424,8 +424,6 @@ DEFINE_Q35_MACHINE(8, 1);
 
 static void pc_q35_machine_8_0_options(MachineClass *m)
 {
-    PCMachineClass *pcmc = PC_MACHINE_CLASS(m);
-
     pc_q35_machine_8_1_options(m);
     compat_props_add(m->compat_props, hw_compat_8_0, hw_compat_8_0_len);
     compat_props_add(m->compat_props, pc_compat_8_0, pc_compat_8_0_len);
